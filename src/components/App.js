74d4a940b9515dbe044f33e49c9e12a042ecb6eb
kush002 <kushagrasingh543@gmail.com>
2022-08-16 01:39:03 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    <Welcome name="Hey !" />
+    <Welcome name="Kushagra" />
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,8 +7,8 @@ const Welcome = (props) => {
 
     return(
         <div>
-            <h1>{name}</h1>
-            <h2>Welcome to Newton School.</h2>
+            <h1>Hey! {name}</h1>
+            <h2>Welcome to Newton School</h2>
         </div>
     )
 
